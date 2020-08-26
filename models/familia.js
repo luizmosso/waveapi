f9ff729d6537f268eec36bc031a6dc1566f58fdf
Luiz Gustavo Rios Moço <luizmosso@gmail.com>
2020-08-26 17:35:59 -0300

    Adding new fields for the family

--- models/familia.js
+++ models/familia.js
@@ -2,21 +2,46 @@ const restful = require('node-restful');
 const mongoose = restful.mongoose;
 
 const Familia = new mongoose.Schema({
-    id: String,
-    ativo: Boolean,
-    dataCadastro: { type: Date, default: Date.now },
-    dataAtivacao: [Date],
-    endereco: String,
-    bairro: String,
-    telefone: Number,
-    documento: String,
-    renda: Number,
-    preco: Number,
-    beneficios: [{nome : String, valor: Number}],
-    membros: [{nome : String, nascimento : Date, escolaridade : String, responsavel : Boolean}],
-    observacao : String,
-    tempoAtendimento : Number,
-    itensDoados: String
+  id: String,
+  ativo: Boolean,
+  dataCadastro: { type: Date, default: Date.now },
+  dataAtivacao: [Date],
+  endereco: String,
+  bairro: String,
+  telefone: Number,
+  documento: String,
+  renda: Number,
+  despesas: Number,
+  preco: Number,
+  beneficios: [{ nome: String, valor: Number }],
+  membros: [
+    {
+      nome: String,
+      nascimento: Date,
+      escolaridade: String,
+      responsavel: Boolean,
+    },
+  ],
+  cronograma: [
+    {
+      number: Number,
+      year: Number,
+      label: String,
+      status: String,
+      disabled: Boolean,
+    },
+  ],
+  observacao: String,
+  tempoAtendimento: Number,
+  itensDoados: String,
+  historicoAtivacao: [
+    {
+      data: { type: Date, default: Date.now },
+      userID: String,
+      nome: String,
+      ativo: Boolean,
+    },
+  ],
 });
 
 module.exports = restful.model('familia', Familia);
