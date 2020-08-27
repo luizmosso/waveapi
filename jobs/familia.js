4369de72a2977cf5b8fcd80b4529dd6d930fca3a
Luiz Gustavo Rios Moço <luizmosso@gmail.com>
2020-08-27 14:11:49 -0300

    teste

--- jobs/familia.js
+++ jobs/familia.js
@@ -1,18 +1,34 @@
 var moment = require('moment');
 var { getFamilias } = require('../controllers/familia');
 
+function DisableFamily(family) {
+  console.log('UMA', family.id);
+}
+
 async function DisableFamiliesByEndOfAttendance() {
-  console.log('disabled'); // Get familias
   const familias = await getFamilias();
   const ativas = familias.filter(({ ativo }) => ativo);
-  ativas.forEach(({ dataCadastro, tempoAtendimento }) => {
+  ativas.forEach((family) => {
+    const { dataCadastro, tempoAtendimento, cronograma } = family;
     const cadastro = moment(dataCadastro);
     const final = moment();
 
-    console.log(
-      tempoAtendimento,
-      final.diff(cadastro, 'months', true) >= parseInt(tempoAtendimento)
-    );
+    // Desativa a família caso o tempo de atendimento tenha terminado
+    if (final.diff(cadastro, 'months', true) >= parseInt(tempoAtendimento)) {
+      DisableFamily(family);
+    } else {
+      for (let index = 0; index < cronograma.length; index++) {
+        const month = cronograma[index];
+        if (index > 0) {
+          const pastMonth = cronograma[index - 1];
+          // Desativa a família caso ela não tenha recolhido a cesta por dois meses seguidos.
+          if (month.status === 'error' && pastMonth.status === 'error') {
+            DisableFamily(family);
+            break;
+          }
+        }
+      }
+    }
   });
 }
 
